fcaaa67e69824ebfd5b9121a3e1c0c6678e68a47
j1ain <jimenasuarezvaldesss@gmail.com>
2026-03-17 15:51:15 +0100

    server.js api bouncer inclusion

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ app.use((req, res, next) => {
         accent: '#e066a3',  
     };
 
-    if (isUnderConstruction && req.query.admin !== process.env.ADMIN_KEY && !req.path.startsWith('/leaderboard') && !req.path.startsWith('/api/scores')) {
+    if (isUnderConstruction && req.query.admin !== process.env.ADMIN_KEY && !req.path.startsWith('/leaderboard') && !req.path.startsWith('/api/')) {
        res.sendFile(path.join(__dirname, 'views', 'wip.html'));
     } else  {
         next(); 
